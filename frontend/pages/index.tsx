2eafb19e6162e50a62b5a1915c3823d8a4bfdd09
Noah Arevalo <noah@Noahs-MBP.hsd1.va.comcast.net>
2023-05-23 10:55:27 -0400

    Nuke repo, implement next.js 13 app folder structure.

--- frontend/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Test from "../components/sections/test"
-
-export default function Home(){
-  return (
-    <main>
-        <Test/>
-    </main>
-  )
-}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,33 +1,18 @@
-const { fontFamily } = require('tailwindcss/defaultTheme');
-
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-    './node_modules/tw-elements/dist/js/**/*.js'
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    screens: {
-      'xs': '414px',
-      'sm': '576px',
-      'md': '768px',
-      'lg': '991px', 
-      'xl': '1200px', 
-      '2xl': '1536px',
-    },
     extend: {
-      fontFamily: {
-        primary: ['var(--lato-font)', ...fontFamily.sans],
-        secondary: ['var(--darumadrop_One-font)', ...fontFamily.sans],
-      },
-      colors: {
-        'black': '#000000', 
-        'white': '#FFFFFF',
-        'dark' : '#1C1C1C'
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
+  plugins: [],
 }
