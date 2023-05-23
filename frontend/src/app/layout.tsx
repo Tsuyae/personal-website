45b5430d61c37856b3f6a039e2e20ab36bc1db2c
Noah Arevalo <noah@Noahs-MBP.hsd1.va.comcast.net>
2023-05-23 11:21:04 -0400

    Import Lato Font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,12 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Lato } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const lato = Lato({ 
+  weight: ['100', '300', '400', '700', '900'], 
+  style: ["normal"], 
+  subsets: ['latin'], 
+  display: 'swap'
+}); 
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={lato.className}>{children}</body>
     </html>
   )
 }
