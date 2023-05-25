6056b8e8bdd23a992b72fb80e8a36238c7b131c5
Noah Arevalo <noah@Noahs-MBP.hsd1.va.comcast.net>
2023-05-24 23:19:05 -0400

    Complete Typing animation Hero

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,6 +14,33 @@ module.exports = {
       'xl': '1200px', 
       '2xl': '1536px',
     },
+    colors : {
+      'dark-gray' : '#1C1C1C',
+      'white': '#FFFFFF',
+      'black': '#000000'
+    },
+    extend: {
+      animation: {
+        cursor: 'cursor .6s linear infinite alternate',
+        type: 'type 1s ease-out .8s 1 normal both'
+      },
+      keyframes: {
+        type: {
+          '0%': { width: '0ch' },
+          '5%, 10%': { width: '1ch' },
+          '15%, 20%': { width: '2ch' },
+          '25%, 30%': { width: '3ch' },
+          '35%, 40%': { width: '4ch' },
+          '45%, 50%': { width: '5ch' },
+          '55%, 60%': { width: '6ch' },
+          '65%, 70%': { width: '7ch' },
+          '75%, 80%': { width: '8ch' },
+          '85%, 90%': { width: '9ch' },
+          '95%': { width: '10ch' },
+          '100%': { width: '11ch' },
+        },
+      },
+    },
   },
   plugins: [],
 }
