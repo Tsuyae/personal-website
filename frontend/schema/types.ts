0b15e046ab8224b604231efae4cc6f9ef3cf1e49
Noah Arevalo <noah@Noahs-MBP.hsd1.va.comcast.net>
2023-06-13 20:40:24 -0400

    Remove experimental type

--- frontend/schema/types.ts
+++ frontend/schema/types.ts
@@ -3,15 +3,10 @@ interface link {
     href : string
 }
 
-type Children<T> = { [text in keyof T & string as `children`]: T[text]  }
-
-type link2 = Children<link>;
-
 export type NavProps = {
     links : link[]
 }
 
 export type NavLinkProps = link & {
     className? : string
-    
 }
