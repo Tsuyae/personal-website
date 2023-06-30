2922f17cb2261f311eb51ba68530ef8808904eea
Noah Arevalo <me@noaharevalo.com>
2023-06-29 22:11:59 -0400

    Remove opacity change on Link

--- frontend/components/elements/social-link.tsx
+++ frontend/components/elements/social-link.tsx
@@ -21,7 +21,7 @@ export default function SocialLink(props : SocialLinkProps) {
         {React.createElement(props.icon, {color: 'white', className: 'h-6 w-7 opacity-40'})}
         {/* create the icon with the fixed height appropriate for this component & pass in respective props. */}
         <Link
-            className='opacity-90 transition duration-100 ease-in-out hover:opacity-60'
+            className='opacity-90'
             href={props.href}
             rel="noopener noreferrer"
             target="_blank">
